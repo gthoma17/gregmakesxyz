a1f89e46d0702db4de3dcb38e35c488318fd6dff
Greg Thomas <gthomas.ctr@kr.af.mil>
2021-07-11 20:26:22 -0600

    Fix updateSite.sh

--- scripts/updateSite.sh
+++ scripts/updateSite.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-ssh gregmakesxyz_gregmakesxyz@ssh.phx.nearlyfreespeech.net -C rm -r "/home/public/*" | true
+ssh gregmakesxyz_gregmakesxyz@ssh.phx.nearlyfreespeech.net -C rm -r /home/public/* | true
 
 # sftp gregmakesxyz_gregmakesxyz@ssh.phx.nearlyfreespeech.net <<< $'put -r public/*'
 
