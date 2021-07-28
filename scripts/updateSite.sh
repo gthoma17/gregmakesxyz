22e6cfd457f7e6155d7c6989825a5baaffda0497
Greg Thomas <gthomas.ctr@kr.af.mil>
2021-07-28 12:23:26 -0600

    Update hosting creds

--- scripts/updateSite.sh
+++ scripts/updateSite.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-ssh gregmakesxyz_gregmakesxyz@ssh.phx.nearlyfreespeech.net -C rm -r /home/public/* | true
+ssh gatlp9_gregmakesxyz@ssh.phx.nearlyfreespeech.net -C rm -r /home/public/* | true
 
 # sftp gregmakesxyz_gregmakesxyz@ssh.phx.nearlyfreespeech.net <<< $'put -r public/*'
 
-rsync --archive --verbose --compress --human-readable --progress --rsh=ssh --recursive public/ gregmakesxyz_gregmakesxyz@ssh.phx.nearlyfreespeech.net:/home/public
+rsync --archive --verbose --compress --human-readable --progress --rsh=ssh --recursive public/ gatlp9_gregmakesxyz@ssh.phx.nearlyfreespeech.net:/home/public
